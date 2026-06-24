36fcb0779dcc5e21db7a723734c13e5c3fac7a54
Marcos-1986 <manducamarcos067@gmail.com>
2026-06-24 18:43:16 -0300

    Agregue usuario_y_socio.py

---
+++ Usuario_y_socio.py
@@ -0,0 +1,31 @@
+class User:
+    def __init__(self, nombre, apellido, dni, correo):
+        self._nombre = nombre
+        self._apellido = apellido
+        self._dni = dni
+        self._correo = correo
+
+    def mostrar_datos(self):
+        return (
+            f"Nombre: {self._nombre}, "
+            f"Apellido: {self._apellido}, "
+            f"DNI: {self._dni}, "
+            f"Correo: {self._correo}"
+        )
+
+
+class Socio(User):
+    def __init__(self, nombre, apellido, dni, correo, nro_socio, fecha_alta):
+        super().__init__(nombre, apellido, dni, correo)
+        self._nro_socio = nro_socio
+        self._fecha_alta = fecha_alta
+
+    def mostrar_datos(self):
+        return (
+            f"Nombre: {self._nombre}, "
+            f"Apellido: {self._apellido}, "
+            f"DNI: {self._dni}, "
+            f"Correo: {self._correo}, "
+            f"Numero de Socio: {self._nro_socio}, "
+            f"Fecha Alta: {self._fecha_alta}"
+        )
