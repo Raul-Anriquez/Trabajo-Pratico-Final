9f5f1de81ee0bc08bcd4420bd3a7404ebb35bc44
Raul <raulanriquez23.com>
2026-06-28 18:47:57 -0300

    Mismo error solucionado

--- Gestion_Prestamos.py
+++ Gestion_Prestamos.py
@@ -1,4 +1,4 @@
-from decorador import registrar_operacion
+from Decorador import registrar_operacion
 
 class GestionPrestamos:
     def __init__(self):
--- Gestion_Usuarios.py
+++ Gestion_Usuarios.py
@@ -1,4 +1,4 @@
-from decorador import registrar_operacion
+from Decorador import registrar_operacion
 
 class GestionUsuarios:
     def __init__(self):
